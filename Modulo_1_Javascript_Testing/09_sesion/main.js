4a11415719fa6147d4b5ebd0d0211c06439a7b28
megaferaguirre <faguirrer@megacable.com.mx>
2025-04-06 19:49:06 -0600

    Sesion 9

---
+++ Modulo_1_Javascript_Testing/09_sesion/main.js
@@ -0,0 +1,11 @@
+import {saludar, despedir, sumar} from"./saludo.js"
+
+const nombre = "Fernando";
+
+console.log(saludar(nombre));
+console.log(despedir(nombre));
+
+console.log(sumar(5,10));
+console.log(restar(10,5));
+console.log(multiplicar(5,10));
+console.log(dividir(10,5));
---
+++ Modulo_1_Javascript_Testing/09_sesion/saludo.js
@@ -0,0 +1,23 @@
+export function saludar(nombre){
+    return `Hola. ${nombre}: Bienvenido a la clase de modulos en JS`
+}
+
+export function despedir(nombre){
+    return `Hola. ${nombre}: Nos vemos en la proxima clase`
+}
+
+export function sumar(a,b){
+    return a + b;
+}
+
+export function restar(a,b){
+    return a - b;
+}
+
+export function multiplicar(a,b){
+    return a * b;
+}
+
+export function dividir(a,b){
+    return a / b;
+}
